ac26d70cf749278530090baf8912a7d31d4ea3a2
nelly476 <nellybulatova12@gmail.com>
2022-11-07 00:22:40 +0300

    create general layout for explore page

---
+++ index.js
@@ -0,0 +1,7 @@
+document.getElementById("search-btn").addEventListener("click", () => {
+  const movieTitle = document.getElementById("search-bar-input").value;
+
+  // fetch(`http://www.omdbapi.com/?s=${movieTitle}&apikey=f6f40030`)
+  //   .then((res) => res.json())
+  //   .then((data) => console.log(data));
+});
